345f4ea6f27d7d86abfec61ba75342c8f43a2343
Matías Nicolás Sepúlveda Ojeda <msepulveda@trisepsoft.onmicrosoft.com>
2025-11-19 19:00:54 -0300

    commit 2

---
+++ src/main/java/com/empresa/mi_proyecto_pruebas/Calculadora.java
@@ -0,0 +1,18 @@
+package com.empresa.mi_proyecto_pruebas;
+
+
+
+public class Calculadora {
+    
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    
+    public int restar(int a, int b) {
+        return a - b;
+    }
+    
+    public boolean esPositivo(int numero) {
+        return numero > 0;
+    }
+}
